bb1fa44455500cc8af835caf7c7cf63f62621794
martenl <martenlosansky@web.de>
2012-09-20 20:44:47 +0200

    Added class GPSList, which will be used to order the GPS data by age and filter out data with a gdop under a certain threshold.

---
+++ src/main/java/spacetechnology/model/GPSDataList.java
@@ -0,0 +1,13 @@
+package spacetechnology.model;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Laptop
+ * Date: 20.09.12
+ * Time: 19:08
+ * To change this template use File | Settings | File Templates.
+ */
+public class GPSDataList {
+
+
+}
